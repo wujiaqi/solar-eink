6858b2dec218bb264e30427a0451c2b5b86e276d
Jiaqi Wu <wujiaqi@JiaqianaliesMac.local>
2023-09-24 13:07:12 -0700

    refactor small display code to use python based puppeteer

--- display_eink_image.py
+++ display_eink_image.py
@@ -4,7 +4,9 @@ import sys
 import os
 from datetime import datetime
 import time
-#picdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), 'pic')
+import asyncio
+import capture_page
+import argparse
 fontdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pic')
 picdir = "/mnt/nfs/eink"
 libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'lib')
@@ -14,11 +16,16 @@ if os.path.exists(libdir):
 import logging
 from waveshare_epd import epd7in5_V2
 import time
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image, ImageDraw, ImageFont
 import traceback
 
-logging.basicConfig(level=logging.DEBUG)
-font18 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 18)
+
+def parse_args():
+    p = argparse.ArgumentParser(description='Test EPD functionality')
+    p.add_argument('-u', '--url', required=True)
+    p.add_argument('-w', '--width', type=int, required=True)
+    p.add_argument('-t', '--height', type=int, required=True)
+    return p.parse_args()
 
 def get_timestamp_str():
     ts = time.time()
@@ -26,42 +33,44 @@ def get_timestamp_str():
     str_datetime = date_time.strftime("%B %d, %Y, %H:%M")
     return str_datetime
 
-#screencap_path = os.path.join(picdir, 'screen_capture.png')
-screencap_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'screenshot.png')
-if os.path.exists(screencap_path) == False:
-    logging.error(f"{screencap_path} not found, is nfs share mounted?")
-    exit()
 
-try:
-    logging.info("epd7in5_V2 Demo")
-    epd = epd7in5_V2.EPD()
-    
-    logging.info("init and Clear")
-    epd.init()
-    epd.Clear()
+def main():
+    font18 = ImageFont.truetype(os.path.join(fontdir, 'Font.ttc'), 18)
+    args = parse_args()
+    screenshot = asyncio.get_event_loop().run_until_complete(capture_page.capture(args.url, args.width, args.height))
 
-    logging.info(f"Displaying {screencap_path}")
-    Himage = Image.open(screencap_path)
-    draw = ImageDraw.Draw(Himage)
-    timestamp_text = get_timestamp_str()
-    draw.text((2, 0), timestamp_text, font = font18, fill = 0)
-    epd.display(epd.getbuffer(Himage))
-    time.sleep(2)
+    try:
+        logging.info("epd7in5_V2 Demo")
+        epd = epd7in5_V2.EPD()
+        
+        logging.info("init and Clear")
+        epd.init()
+        epd.Clear()
 
-    logging.info("Goto Sleep...")
-    epd.sleep()
-    
-except IOError as e:
-    logging.info(e)
-    
-except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
-    epd7in5_V2.epdconfig.module_exit()
-    exit()
+        logging.info(f"Displaying image...")
+        draw = ImageDraw.Draw(screenshot)
+        timestamp_text = get_timestamp_str()
+        draw.text((2, 0), timestamp_text, font = font18, fill = 0)
+        epd.display(epd.getbuffer(screenshot))
+        time.sleep(2)
 
-except Exception as e:
-    logging.info(e)
-    epd7in5_V2.epdconfig.module_exit()
-    exit()
+        logging.info("Goto Sleep...")
+        epd.sleep()
+        
+    except IOError as e:
+        logging.info(e)
+        
+    except KeyboardInterrupt:    
+        logging.info("ctrl + c:")
+        epd7in5_V2.epdconfig.module_exit()
+        exit()
 
+    except Exception as e:
+        logging.info(e)
+        epd7in5_V2.epdconfig.module_exit()
+        exit()
+
+
+if __name__ == '__main__':
+    main()
 
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 now=$(date)
 echo "${now}: Starting cron run..."
 cd "$(dirname "$0")"
-/usr/bin/node capture_solar_grafana.js -w 800 -h 600 -u "https://grafana.jiaqiwu.io/d/sPcihT2Vk/solar-e-ink?orgId=1&kiosk"
-/usr/bin/python display_eink_image.py
-rm screenshot.png
+# /usr/bin/node capture_solar_grafana.js -w 800 -h 600 -u "https://grafana.jiaqiwu.io/d/sPcihT2Vk/solar-e-ink?orgId=1&kiosk"
+/usr/bin/python display_eink_image.py -u 'https://grafana.jiaqiwu.io/d/sPcihT2Vk/solar-e-ink?orgId=1&kiosk' -w 800 -h 600
+# rm screenshot.png
 
