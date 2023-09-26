fba22265bc59c3ae799d7103f085e0a4e0afb15e
Jiaqi Wu <wooqiaoqi@gmail.com>
2023-09-26 16:25:36 -0700

    use abs dir

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 now=$(date)
-source eink/bin/activate
+source /home/wujiaqi/git/webepd/eink/bin/activate
 echo "${now}: Starting cron run..."
 cd "$(dirname "$0")"
 python display_eink_image.py -u 'https://grafana.jiaqiwu.io/d/sPcihT2Vk/solar-e-ink?orgId=1&kiosk'
