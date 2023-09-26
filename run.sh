319bf207f87c12566660a0fa83c5a96a2be1cdd4
Jiaqi Wu <wooqiaoqi@gmail.com>
2023-09-26 16:40:53 -0700

    fix the run script

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ now=$(date)
 source /home/wujiaqi/git/webepd/eink/bin/activate
 echo "${now}: Starting cron run..."
 cd "$(dirname "$0")"
-python display_eink_image.py -u 'https://grafana.jiaqiwu.io/d/sPcihT2Vk/solar-e-ink?orgId=1&kiosk'
+python display_eink_image.py -u 'https://grafana.jiaqiwu.io/d/sPcihT2Vk/solar-e-ink?orgId=1&kiosk' -e epd7in5_V2
 deactivate
 
